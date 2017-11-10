74a6aa23dda6543908cf7ee7f73e6f8629aa35b0
Michael Myung <michael.h.myung@gmail.com>
2017-11-10 06:06:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Code your solution in this file!
+function calculateVertical(x,y){
+  if (x>y) {
+    (x-y)*264
+  }
+  else if (y>x) {
+    (y-x)*264
+  }
+  else {
+    return "Please type in digits (e.g. 42, 37)"
+  }
+}
+
 function distanceFromHqInBlocks(block){
   return 42 - block
 }
