b6648ecc677d83cf59bde6227403e17abee8866f
Michael Myung <michael.h.myung@gmail.com>
2017-11-10 06:21:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,24 @@ function distanceFromHqInBlocks(block){
 function distanceFromHqInFeet(block){
   if (block >42) {
     return (block-42)*264
-  } 
+  }
   else if (block < 42) {
     return (42-block)*264
-  } 
+  }
   else {
     return "Please input a valid street number."
   }
 }
+
+function distanceTravelledInFeet(start,end) {
+  if (start>end) {
+    return (start-end)*264
+  }
+  else if (end>start){
+    return (end-start)*264
+  }
+  else {
+    return "Please input valid street numbers."
+  }
+}
+
