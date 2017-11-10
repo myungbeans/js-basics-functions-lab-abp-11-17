7c6cb6cedd5dfa971d12f6195fed5b0363533304
Michael Myung <michael.h.myung@gmail.com>
2017-11-10 06:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,3 +32,16 @@ function distanceTravelledInFeet(start,end) {
     return "Please input valid street numbers."
   }
 }
+
+function calculatesFarePrice(x,y) {
+  let distance = Math.abs((x-y)*264)
+  if (distance<=400) {
+    return 0
+  } else if (distance>400 && distance<=2000) {
+    return distance*.02
+  } else if (distance>2000 && distance <=2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
